67645eb3ceca8cf97c40b87823f4de1985d2494d
Wiley Welch <wileyjwelch1@gmail.com>
2025-03-24 13:42:51 -0600

    Had to fix the file paths for the code to deploy correctly

--- tweeter-web-starter/tweeter-server/src/lambda/feed/GetStoryLambda.ts
+++ tweeter-web-starter/tweeter-server/src/lambda/feed/GetStoryLambda.ts
@@ -1,12 +1,12 @@
 import { GetFeedRequest } from 'tweeter-shared/src/model/net/request/GetFeedRequest';
 import { GetFeedResponse } from 'tweeter-shared/src/model/net/response/GetFeedResponse';
-import { StatusService } from '../../../model/service/StatusService';
+import { StatusService } from '../../model/service/StatusService';
 
 export const handler = async (
   requests: GetFeedRequest
 ): Promise<GetFeedResponse> => {
   const statusService = new StatusService();
-  const [statuses, hasMore] = await statusService.loadMoreFeed(
+  const [statuses, hasMore] = await statusService.loadMoreStory(
     requests.token,
     requests.userAlias,
     requests.pageSize,

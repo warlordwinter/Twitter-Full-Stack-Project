736ca8d6504e2a3b5c6cb153ba86ffe03955e326
Wiley Welch <wileyjwelch1@gmail.com>
2025-03-25 12:39:08 -0600

    made it so logout lambda uses a string

--- tweeter-web-starter/tweeter-server/src/lambda/authentication/LogoutLambda.ts
+++ tweeter-web-starter/tweeter-server/src/lambda/authentication/LogoutLambda.ts
@@ -1,12 +1,17 @@
 import { AuthenticationService } from '../../model/service/AuthenticationService';
 import { LogoutRequest } from 'tweeter-shared/src/model/net/request/LogoutRequest';
 import { LogoutResponse } from 'tweeter-shared/src/model/net/response/LogoutResponse';
+import { AuthTokenDto } from 'tweeter-shared/src/model/dto/AuthTokenDto';
 
 export const handler = async (
   requests: LogoutRequest
 ): Promise<LogoutResponse> => {
   const authenticationService = new AuthenticationService();
-  await authenticationService.logout(requests.authToken);
+  const authTokenDto: AuthTokenDto = {
+    token: requests.authToken,
+    timestamp: Date.now(),
+  };
+  await authenticationService.logout(authTokenDto);
   return {
     success: true,
     message: null,
